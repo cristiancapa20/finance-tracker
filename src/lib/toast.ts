5f091aec96250dd17da534b1e57fbb59cfb936e6
Tu Nombre <tu.email@ejemplo.com>
2026-03-08 18:45:44 -0500

    feat: change UI Toast sileo

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -1,8 +1,16 @@
 import { sileo } from "sileo";
 
+const darkStyles = {
+  fill: "#171717",
+  styles: {
+    title: "text-white!",
+    description: "text-white/75!",
+  },
+};
+
 export const toast = {
   success: (opts: { title: string; description?: string }) =>
-    sileo.success(opts),
+    sileo.success({ ...darkStyles, ...opts }),
   error: (opts: { title: string; description?: string }) =>
-    sileo.error(opts),
+    sileo.error({ ...darkStyles, ...opts }),
 };
